b6e541a00f4fd3da1569e9ef4d88a3e49d57be96
aabaroq85-byte <aabaroq85@gmail.com>
2026-07-31 23:15:49 +0900

    Create 1_ZF_Forex_Engine.py

---
+++ pages/1_ZF_Forex_Engine.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="ZF Forex Engine", page_icon="📈", layout="centered")
+st.title("📈 ZF Forex Engine")
+st.info("Halaman ZF Forex Engine Siap Digunakan.")
